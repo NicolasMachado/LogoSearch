d40880d778e3dbeb39ef9ae201527eb51abbea49
Nicolas Machado <nicoma63@gmail.com>
2017-02-25 12:37:00 +0100

    Final Cleanup

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 $(function () {
 	init();
+	// when option changed
 	$("select").on("change", function() {
 		highlight($(".pr-type").find(":selected").val(), $(".lvl-ed").find(":selected").val());
 	});
+	// when form submitted (after reload)
 	$("#search-form").on("submit", function(e) {
 		e.preventDefault();
 		highlight($(".pr-type").find(":selected").val(), $(".lvl-ed").find(":selected").val());
@@ -10,12 +12,14 @@ $(function () {
 });
 
 function init () {
+	// initialize all images to fade
 	$.each($(".img-container"), function() {
 		$(this).css("background-image", "url(\"images/" + $(this).data("img") + "-fade.png\")");
 	});
 }
 
 function highlight (programType, lvlEducation) {
+	// select which image to highlight
 	$.each($(".img-container"), function() {
 		if (($(this).data("prgrm") === programType || programType === "all") && ($(this).data("lvled") === lvlEducation || lvlEducation === "all")) {
 			$(this).css("background-image", "url(\"images/" + $(this).data("img") + "-color.png\")");
